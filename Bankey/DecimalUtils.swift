3aa5e618fa6fc7d3cbc484b6df2d4098b5ec1ed9
Oscar Cristaldo <fabianskier@icloud.com>
2022-06-11 22:33:44 -0400

    CO-0014: Add CurrencyFormatter
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

---
+++ Bankey/DecimalUtils.swift
@@ -0,0 +1,14 @@
+//
+//  DecimalUtils.swift
+//  Bankey
+//
+//  Created by Oscar Cristaldo on 2022-06-11.
+//
+
+import Foundation
+
+extension Decimal {
+    var doubleValue: Double {
+        return NSDecimalNumber(decimal: self).doubleValue
+    }
+}
