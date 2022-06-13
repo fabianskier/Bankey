55e9c7b39c8985032d488f4e95208b02c30ecc9b
Oscar Cristaldo <fabianskier@icloud.com>
2022-06-12 20:05:07 -0400

    CO-0018: Logout and toggle password
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

---
+++ Bankey/NSNotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NSNotificationName.swift
+//  Bankey
+//
+//  Created by Oscar Cristaldo on 2022-06-12.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let logout = Notification.Name("Logout")
+}
